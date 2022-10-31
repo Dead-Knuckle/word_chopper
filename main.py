0fbec1fba4d4d2d5f1d54c49ab4b6fd4251d1e9a
Rewired <97414090+Dead-Knuckle@users.noreply.github.com>
2022-10-31 17:44:52 -0400

    Update main.py
    Add Comments and KeyBoardInterrupt Error handling

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 #Importing requried modules 
 import maskpass, os
  
-#Clearing the Termial 
+#Different OS use different commands to clear the screen, linux:clear win:cls 
 clearing_screen = 'cls'
-os.system(clearing_screen)
+
+#Defining The Correct Password and The Password Attempt amount 
 correct_password = 'password1'
 password_attempt = 11
+
+
+#Clearing the Screen
+os.system(clearing_screen)
+
+#Defining password loop
 while True:
-    password_input = maskpass.askpass(prompt="Password: ", mask="*")
-    if password_input == correct_password:
-        break
-    password_attempt -= 1
-    print(f'Wrong password, you have {password_attempt} remaining')
-    if password_attempt == 0:
-        os.system(clearing_screen)
-        print('You have run out of password attempt, goodbye')
-        exit()
+
+    #Checking if the user trys to bypass the password with a keyboard interrupt
+    try:
+
+        #Getting the users password input with a security mask
+        password_input = maskpass.askpass(prompt="Password: ", mask="*")
+        
+        #Checking if password is the correct password, if so we break from loop
+        if password_input == correct_password:
+            break
+
+        #Else, we subtract one from their attempts , and print it to the user 
+        password_attempt -= 1
+        print(f'Wrong password, you have {password_attempt} remaining')
+
+        #If the user has run out of password attempts then we clear the screen and quit the program
+        if password_attempt == 0:
+            os.system(clearing_screen)
+            print('You have run out of password attempt, goodbye')
+            exit()
+
+    except KeyboardInterrupt:
+        
+        #A taunting message is displayed if the user tries to use KeyboardInterrupt, and the loop continues
+        print("Nice try, it won't be that easy")
+        continue
